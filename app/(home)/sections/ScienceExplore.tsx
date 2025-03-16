bff1180edbde74ec7a37c00c3b56236c8a011c26
albertfredy7 <albertfredy007@gmail.com>
2025-03-16 16:11:29 +0530

    fix:science explore card layout

--- app/(home)/sections/ScienceExplore.tsx
+++ app/(home)/sections/ScienceExplore.tsx
@@ -2,7 +2,7 @@ import ScienceCard from '../components/ScienceCard';
 
 export default function ScienceExplore() {
   return (
-    <section className="science-explore container mx-auto px-4 sm:px-4 lg:px-6 py-10">
+    <section className="science-explore container  mx-auto px-4 sm:px-4 lg:px-6 py-10">
       <ScienceCard />
     </section>
   );
