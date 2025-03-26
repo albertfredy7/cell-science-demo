eaf760614fa47914b8b2ef0d529d47148d6e7cc0
albertfredy7 <albertfredy007@gmail.com>
2025-03-26 13:58:03 +0530

    fix: temporarily hide Testimonials and Blog sections for future implementation

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,7 +2,6 @@ import About from './sections/About';
 import About2 from './sections/About2';
 import About3 from './sections/About3';
 import About4 from './sections/About4';
-import Blog from './sections/Blog';
 import Contact from './sections/Contact';
 import Hallmarks from './sections/Hallmarks';
 import Hero from './sections/Hero';
@@ -10,7 +9,6 @@ import Mitochondria from './sections/Mitochondria';
 import Pipeline from './sections/Pipeline';
 import ScienceExplore from './sections/ScienceExplore';
 import Team from './sections/Team';
-import Testimonials from './sections/Testimonials';
 
 export default function Home() {
     return (
@@ -25,8 +23,9 @@ export default function Home() {
             <Hallmarks/>
             <Pipeline/>
             <Team/>
-            <Testimonials/>
-            <Blog/>
+            {/* currently hiding, will add after implementation */}
+            {/* <Testimonials/>
+            <Blog/> */}
             <Contact/>
         </>
     );
