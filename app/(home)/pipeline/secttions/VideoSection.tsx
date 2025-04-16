711a032bef59d395deebda285ef95e5e8926396b
albertfredy7 <albertfredy007@gmail.com>
2025-04-16 13:05:34 +0530

    fix: update text in VideoSection for clarity and emphasis

--- app/(home)/pipeline/secttions/VideoSection.tsx
+++ app/(home)/pipeline/secttions/VideoSection.tsx
@@ -3,7 +3,7 @@ import TextReveal from '@/components/TextReveal';
 import ImageCanvas from '../components/ImageCanvas';
 
 export default function Home() {
-  
+
 
     return (
         <section >
@@ -25,14 +25,17 @@ export default function Home() {
                 <div className="max-w-5xl px-6">
                     <TextReveal
                         className="text-center text-white text-3xl md:text-4xl lg:text-4xl font-light "
-                        // specialWords={'the ovary serves as the nest, and the homeostasis in the ovarian microenvironment changes with age'}
+                    // specialWords={'the ovary serves as the nest, and the homeostasis in the ovarian microenvironment changes with age'}
                     >
-                        Cellscience Biotech focuses on the very first organs that age in humans—the female reproductive organs. We are shifting the perspective of ovarian biology it’s not just about the egg, the ovary serves as the nest, and the homeostasis in the ovarian microenvironment changes with age.
+                        Cellscience Biotech targets on the very first organs that age in humans—the female reproductive organs. We are shifting the perspective of ovarian biology; it’s not just the egg, the ovary - Nest, and the homeostasis in the ovarian microenvironment changes with age.
+
                     </TextReveal>
                     <TextReveal
                         className="text-center text-white text-3xl md:text-4xl lg:text-4xl font-light"
                     >
+
                         Targeting these newly identified hallmarks can enhance the quality of follicles by restoring the freshness of youth in the reproductive tissues.
+
                     </TextReveal>
                 </div>
             </section>
